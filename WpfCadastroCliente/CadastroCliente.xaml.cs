12616435f916b3fffa2ee6afaed083c48655210d
BarbaraIaguita <126489529+BarbaraIaguita@users.noreply.github.com>
2023-05-01 20:15:19 -0400

    WpfCadastroCliente

---
+++ WpfCadastroCliente/CadastroCliente.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfCadastroCliente
+{
+    /// <summary>
+    /// Lógica interna para CadastroCliente.xaml
+    /// </summary>
+    public partial class CadastroCliente : Window
+    {
+        public CadastroCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNome_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CbxMunicipio_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void txtTelefone_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TxtxNumero_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CbxMunicipio_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
