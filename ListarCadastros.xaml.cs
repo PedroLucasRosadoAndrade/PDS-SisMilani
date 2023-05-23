b2db7e774d71f72862b4abf35665bc9d4d4697d0
PedroLucasRosadoAndrade <126489915+PedroLucasRosadoAndrade@users.noreply.github.com>
2023-05-23 10:01:25 -0400

    relacionando as telas

--- ListarCadastros.xaml.cs
+++ ListarCadastros.xaml.cs
@@ -23,5 +23,35 @@ namespace ProjetoDePDS3_A
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ListarFornecedores form = new ListarFornecedores();
+            form.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ListarFilme form =  new ListarFilme();
+            form.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ListarFuncionarios form = new ListarFuncionarios();
+            form.Show();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+             ListarProdutores form = new ListarProdutores();    
+            form.Show();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            ListarCliente form = new ListarCliente();   
+            form.Show();
+        }
     }
 }
